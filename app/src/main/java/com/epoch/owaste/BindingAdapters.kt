51748dd240eca50eb49aeab5aee0f65ba9d3f732
Eltin Yang <itsmyemail0725@gmail.com>
2019-09-19 19:31:42 +0800

    Create BindingAdapters.kt

---
+++ app/src/main/java/com/epoch/owaste/BindingAdapters.kt
@@ -0,0 +1,9 @@
+package com.epoch.owaste
+
+import androidx.recyclerview.widget.RecyclerView
+import com.epoch.owaste.data.RewardCard
+
+
+fun bindRecyclerViewWithRewardCards(recyclerView: RecyclerView, rewardCards: RewardCard) {
+    rewardCards
+}
