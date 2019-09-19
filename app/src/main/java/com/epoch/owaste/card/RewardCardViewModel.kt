b36419dc3d724bf6ca15bdbc297998ceda37c7d5
Eltin Yang <itsmyemail0725@gmail.com>
2019-09-19 19:53:20 +0800

    call getAllRewardCardFromFirestore() in RewardCardViewModel init {}

--- app/src/main/java/com/epoch/owaste/card/RewardCardViewModel.kt
+++ app/src/main/java/com/epoch/owaste/card/RewardCardViewModel.kt
@@ -1,4 +1,23 @@
 package com.epoch.owaste.card
 
-class RewardCardViewModel {
+import android.util.Log.i
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.epoch.owaste.data.OwasteRepository
+import com.epoch.owaste.data.RewardCard
+
+class RewardCardViewModel : ViewModel() {
+
+    val TAG = "Eltin_" + this.javaClass.simpleName
+
+    val _allRewardCards = MutableLiveData<List<RewardCard>>()
+    val allRewardCards: LiveData<List<RewardCard>>
+        get() = _allRewardCards
+
+    init {
+        OwasteRepository.getAllRewardCardFromFirestore()
+        _allRewardCards.value = OwasteRepository.allRewardCards.value
+        i(TAG, "LiveData<List<RewardCard>> value = ${allRewardCards.value}")
+    }
 }
